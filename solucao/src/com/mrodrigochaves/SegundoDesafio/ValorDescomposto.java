bd19d5544b5d6ef9af345ddf88c8fa93cb724957
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-07-13 21:02:38 -0300

    adicionando comentarios

--- solucao/src/com/mrodrigochaves/SegundoDesafio/ValorDescomposto.java
+++ solucao/src/com/mrodrigochaves/SegundoDesafio/ValorDescomposto.java
@@ -6,13 +6,22 @@ import java.util.Scanner;
 public class ValorDescomposto {
     
         public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        
+        // Cria um objeto Scanner para ler os dados de entrada
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);//Locale.US para usar o ponto como separador de casas decimais
+
+
+        // Declaração de variáveis
         double valor = scan.nextDouble();
 
+        // Declaração de vetores
         double[] notas = {100, 50, 20, 10, 5, 2};
         double[] moedas = {1, 0.5, 0.25, 0.1, 0.05, 0.01};
 
+        
         System.out.println("NOTAS:");
+
+        // Laço de repetição para percorrer o vetor notas
         for (double nota : notas) {
             int quantidade = (int) (valor / nota);
             valor %= nota;
@@ -20,12 +29,15 @@ public class ValorDescomposto {
         }
 
         System.out.println("MOEDAS:");
+
+        // Laço de repetição para percorrer o vetor moedas
         for (double moeda : moedas) {
             int quantidade = (int) (valor / moeda);
             valor %= moeda;
             System.out.printf("%d moeda(s) de R$ %.2f%n", quantidade, moeda);
         }
 
+        // Fecha o objeto Scanner
         scan.close();
     }
 }
